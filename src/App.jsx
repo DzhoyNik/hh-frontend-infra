85adb49292b0c28fdcf17c19b6557b07e9bb10c7
Anton Mikhalev <DzhoyNik@yandex.ru>
2026-03-13 19:42:05 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const quotes = [
+    "Сначала npm install, потом паника",
+    "Каждый баг — это новая фича",
+    "Если работает — не трогай",
+    "Сегодня ты не баг, ты фича"
+  ]
+
+  const foo = "bar"
+  foo = "baz"
+  
+  const [ quote, setQuotes ] = useState(quotes[0])
+
+  setInterval(() => {
+    let index = Math.floor(Math.random(0, 3) * quotes.length)/
+    setQuotes(quotes[index])
+  }, 5000);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Рандомные цитаты</h2>
+        <h1>{quote}</h1>
+        <p>Обновляются каждые 5 секунд</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
